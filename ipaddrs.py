e31fb8e12d6bc32b9fca2afbe6df0c6a9a4690c6
Muhannad Alghamdi <muhannadengineer@gmail.com>
2019-05-03 18:54:56 +0300

    Do not range duplicated ips

--- ipaddrs.py
+++ ipaddrs.py
@@ -2,17 +2,22 @@ import re
 
 
 def load_ipaddrs(path):
-    with open('hosts', 'r') as f:
+    with open(path, 'r') as f:
         contents = f.read()
-        regex = r'([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})'
+        regex = r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' \
+                r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' \
+                r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' \
+                r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+
         ipaddrs = re.findall(pattern=regex, string=contents)
+        print(f'loaded {len(ipaddrs)} ip addresses')
         return ipaddrs
 
 
-def group_ipaddrs(ipaddrs, cidr):
+def group_ipaddrs(ipaddrs, octet):
     group = [ipaddrs[0]]
     for ip in ipaddrs[1:]:
-        if ip[:cidr] == group[-1][:cidr]:
+        if ip[:octet] == group[-1][:octet]:
             group.append(ip)
         else:
             yield group
@@ -20,22 +25,32 @@ def group_ipaddrs(ipaddrs, cidr):
     yield group
 
 
-def create_range(l):
-    first = last = l[0]
-    for next in l[1:]:
+def range_ipaddrs(ipaddrs):
+    first = last = ipaddrs[0]
+    for next in ipaddrs[1:]:
         if int(next[3]) - 1 == int(last[3]):
             last = next
         else:
-            yield '.'.join(first) + '-' + last[3]
+            if first == last:
+                yield '.'.join(first)
+            else:
+                yield '.'.join(first) + '-' + last[3]
             first = last = next
-    yield '.'.join(first) + '-' + last[3]
+    if first == last:
+        yield '.'.join(first)
+    else:
+        yield '.'.join(first) + '-' + last[3]
 
 
-def get_ranged_ipadds(ipaddrs):
-    ipaddrs = sorted(ipaddrs)
-    for ip_group in group_ipaddrs(ipaddrs=ipaddrs, cidr=3):
-        for range in create_range(ip_group):
-            yield range
+def separate_list(ipaddrs, max_len):
+    list = []
+    for range in ipaddrs:
+        if (len_list(list) + len(range)) <= max_len:
+            list.append(range)
+        else:
+            yield list
+            list = []
+    yield list
 
 
 def len_list(list):
@@ -46,21 +61,20 @@ def len_list(list):
     return max
 
 
-def seperate_list(ipaddrs, max_len):
-    list = []
-    for range in ipaddrs:
-        if (len_list(list) + len(range)) <= max_len:
-            list.append(range)
-        else:
-            yield list
-            list = []
+def get_ranged_ipadds(ipaddrs):
+    ipaddrs = sorted(ipaddrs)
+    for grouped_ipaddrs in group_ipaddrs(ipaddrs=ipaddrs, octet=3):
+        for ranged_ipaddrs in range_ipaddrs(ipaddrs=grouped_ipaddrs):
+            yield ranged_ipaddrs
 
 
 def main():
     ipaddrs = load_ipaddrs(path='eai')
-    ipranges = [range for range in get_ranged_ipadds(ipaddrs=ipaddrs)]
-    for list in seperate_list(ipaddrs=ipranges, max_len=200):
-        print(','.join(list))
+    ranged_ipaddrs_gen = get_ranged_ipadds(ipaddrs=ipaddrs)
+    separated_ipaddrs_gen = separate_list(ipaddrs=ranged_ipaddrs_gen, max_len=350)
+
+    for range in separated_ipaddrs_gen:
+        print(','.join(range))
 
 
 if __name__ == '__main__':
