cc6d137fe3621a9f7683f7ba4ed27d011e335603
Muhannad Alghamdi <muhannadengineer@gmail.com>
2019-10-06 11:41:54 +0300

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='netrange',
-      version='0.0.5',
+      version='0.0.6',
       description='Script to range multiple IPs and ports',
       url='https://github.com/muhannadengineer/iprange',
       author='Muhannad Alghamdi',
--- tests/test.py
+++ tests/test.py
@@ -4,4 +4,5 @@ import json
 ips_list = ['172.25.12.195', '172.25.12.196', '172.25.12.197', '172.25.12.199']
 
 ips = iprange.load_ipaddrs(from_args=ips_list, verbose=True)
+print(ips)
 print(iprange.dumps_ipaddrs(ipaddrs=ips))
