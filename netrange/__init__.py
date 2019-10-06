4e4dc328f66d4def69cdf210d3b8c2ed4ad280da
Muhannad Alghamdi <muhannadengineer@gmail.com>
2019-10-06 18:11:36 +0300

    Dump ips and port lists

--- netrange/__init__.py
+++ netrange/__init__.py
@@ -138,3 +138,21 @@ def dumps_ports(ports, max_len=None, verbose=False):
         separated_ports = separate_list(from_list=ranged_ports, max_len=max_len)
         return '\n'.join([','.join(ports) for ports in separated_ports])
     return '\n'.join([ports for ports in ranged_ports])
+
+
+def dump_ipaddrs(ipaddrs, max_len=None, verbose=False):
+    # TODO: verify ipaddrs contains at least one ip
+    ranged_ipaddrs = get_ranged_ipadds(ipaddrs=ipaddrs, verbose=verbose)
+    if max_len is not None:
+        separated_ipaddrs = separate_list(from_list=ranged_ipaddrs, max_len=max_len)
+        return [','.join(ipaddrs) for ipaddrs in separated_ipaddrs]
+    return [ipaddrs for ipaddrs in ranged_ipaddrs]
+
+
+def dump_ports(ports, max_len=None, verbose=False):
+    # TODO: verify ports contains at least one port
+    ranged_ports = get_ranged_ports(ports=ports, verbose=verbose)
+    if max_len is not None:
+        separated_ports = separate_list(from_list=ranged_ports, max_len=max_len)
+        return [','.join(ports) for ports in separated_ports]
+    return [ports for ports in ranged_ports]
--- tests/test.py
+++ tests/test.py
@@ -4,8 +4,10 @@ import json
 ips_list = ['172.25.12.195', '172.25.12.196', '172.25.12.197', '172.25.12.199', 'dfdfd']
 
 ips = netrange.load_ipaddrs(from_args=ips_list)
-print(ips)
+# print(ips)
 
-ranges = netrange.dumps_ipaddrs(ipaddrs=ips)
-
+ranges = netrange.dumps_ipaddrs(ips, 200)
 print(ranges)
+
+dump = netrange.dump_ipaddrs(ips, 200)
+print(dump)
