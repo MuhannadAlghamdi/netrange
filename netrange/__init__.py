a7e48fa52be9a826153f9db60cf1b9b6f1225ce9
Muhannad Alghamdi <muhannadengineer@gmail.com>
2019-10-07 00:48:03 +0300

    Inc version

--- netrange/__init__.py
+++ netrange/__init__.py
@@ -5,7 +5,7 @@ from netrange.core import dumps_ports
 from netrange.core import load_ipaddrs
 from netrange.core import load_ports
 
-__version__ = '0.0.0'
+__version__ = '0.0.9'
 __all__ = [
     'dump_ipaddrs', 'dump_ports', 'dumps_ipaddrs', 'dumps_ports', 'load_ipaddrs', 'load_ports', '__author__'
 ]
