93aba3a5919ffc9027f3e7fe74d013e030fe6c5f
Muhannad Alghamdi <muhannadengineer@gmail.com>
2019-10-11 18:48:45 +0300

    Combine ip and port modules into load and dump

--- tests/test_ip.py
+++ tests/test_ip.py
@@ -1,6 +1,6 @@
 import json
 from os import path
-from netrange import ip
+import netrange
 
 this_directory = path.abspath(path.dirname(__file__))
 
@@ -8,12 +8,12 @@ this_directory = path.abspath(path.dirname(__file__))
 ips_list = ['172.25.12.195', '172.25.12.196', '172.25.12.197', '172.25.12.199', 'dfdfd']
 
 
-def test_load_ipaddrs_from_list():
-    ips = ip.loads(from_args=ips_list)
+def test_load_ips_from_string():
+    ips = netrange.load_ips_from_string(*ips_list)
     assert len(ips) > 0
 
 
-def test_load_ipaddrs_from_file():
+def test_load_ips_from_file():
     file = open(file=f'{this_directory}/ips', mode='r')
-    ips = ip.load(from_file=file)
+    ips = netrange.load_ips_from_file(file=file)
     assert len(ips) > 0
