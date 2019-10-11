bb7167dc7af894ffc5d60a3ba7ac6f152da03f87
Muhannad Alghamdi <muhannadengineer@gmail.com>
2019-10-11 04:39:31 +0300

    Test load ip

--- netrange/ip/load.py
+++ netrange/ip/load.py
@@ -2,15 +2,6 @@ from netrange._parser import parse_ipaddrs
 
 
 def load(from_file, verbose=False):
-    """
-    Load the CoNLL-U source in a string into a Conll object.
-    Args:
-        source: The CoNLL-U formatted string.
-    Returns:
-        A Conll object equivalent to the provided source.
-    Raises:
-        ParseError: If there is an error parsing the input into a Conll object.
-    """
     contents = from_file.read()
     ipaddrs = parse_ipaddrs(contents=contents)
 
--- tests/test_ip.py
+++ tests/test_ip.py
@@ -1,6 +1,6 @@
 import json
 from os import path
-import netrange
+from netrange import ip
 
 this_directory = path.abspath(path.dirname(__file__))
 
@@ -9,10 +9,11 @@ ips_list = ['172.25.12.195', '172.25.12.196', '172.25.12.197', '172.25.12.199', 
 
 
 def test_load_ipaddrs_from_list():
-    ips = netrange.load_ipaddrs(from_args=ips_list)
+    ips = ip.loads(from_args=ips_list)
     assert len(ips) > 0
 
 
 def test_load_ipaddrs_from_file():
-    ips = netrange.load_ipaddrs(from_file=f'{this_directory}/ips')
+    file = open(file=f'{this_directory}/ips', mode='r')
+    ips = ip.load(from_file=file)
     assert len(ips) > 0
